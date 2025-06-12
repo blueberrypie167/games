220f56be9a9dac05727adc18ddcbe2e4931b5821
blueberrypie167 <82982605+blueberrypie167@users.noreply.github.com>
2025-06-12 13:16:47 +0300

    Update rockpaperscissor.cpp

--- rockpaperscissor.cpp
+++ rockpaperscissor.cpp
@@ -5,15 +5,45 @@ using namespace std;
 
 int main(){
   int input, cinput;
-  cout << "Pick a weapon!" << endl << "Rock: 1, Paper: 2, Scissors:3" << endl;
+  std::string moves[4] = {"Rock", "Paper", "Scissors"};
+  
+  cout << "Pick a Move!" << endl << "Rock: 1, Paper: 2, Scissors:3" << endl;
   cin >> input;
+  --input;
 // generate a random number each time the program runs from 0 to 2 (3 numbers)
   srand(time(0));
   cinput = rand() % 2;
-
+    cout << "You picked: " << moves[input] << endl;
+    cout << "Your opponent picked: " << moves[cinput] << endl;
 // the comparison algorithm
+    if (cinput == input){
+        cout << "Tie!";
+    }
   switch(input){
-    
+    case 0:
+        if(cinput == 1){
+            cout << "You Lose!";
+        }
+        if(cinput == 2){
+            cout << "You Win!";
+        }
+        break;
+    case 1:
+        if(cinput == 0){
+            cout << "You Win!";
+        }
+        if(cinput == 2){
+            cout << "You Lose!";
+        }
+        break;
+    case 2:
+        if(cinput == 0){
+            cout << "You Lose!";
+        }
+        if(cinput == 1){
+            cout << "You Win!";
+        }
+        break;
   }
   
 }
